7104419f07ec80f377d8e10f95c7f7ac979359e6
IIIByoIII <robertkiss26@gmail.com>
2017-06-01 16:49:58 +0300

    Add empty Mushrooms project

---
+++ Mushrooms/Mushrooms.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mushrooms
+{
+    [TestClass]
+    public class Mushrooms
+    {
+        [TestMethod]
+        public void NumberOfRedMushrooms()
+        {
+        }
+    }
+}
---
+++ Mushrooms/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Mushrooms")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mushrooms")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c7d0bcb9-efdd-423f-9206-1d8ed84fa2ae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
