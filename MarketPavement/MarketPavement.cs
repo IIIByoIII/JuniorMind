8e92e6ba6555f484983173755a623be1689958f5
IIIByoIII <robertkiss26@gmail.com>
2017-06-01 17:00:57 +0300

    Add empty MarketPavement project

---
+++ MarketPavement/MarketPavement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MarketPavement
+{
+    [TestClass]
+    public class MarketPavement
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
