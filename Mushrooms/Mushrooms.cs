4c32d7b8608f3edcccf76b1970954d6bd7ef95e0
IIIByoIII <robertkiss26@gmail.com>
2017-06-01 16:54:00 +0300

    First test of Mushrooms

--- Mushrooms/Mushrooms.cs
+++ Mushrooms/Mushrooms.cs
@@ -9,6 +9,14 @@ namespace Mushrooms
         [TestMethod]
         public void NumberOfRedMushrooms()
         {
+            int redMushrooms = CalculateRedMushrooms(8, 3);
+            Assert.AreEqual(6, redMushrooms);
+        }
+
+        int CalculateRedMushrooms(int totalNumberOfMushrooms, int timesMoreRedMushrooms)
+        {
+            int redMushrooms = timesMoreRedMushrooms * totalNumberOfMushrooms / (1 + timesMoreRedMushrooms);
+            return redMushrooms;
         }
     }
 }
