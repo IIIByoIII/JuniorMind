0982f0fe58b5d790ecac102c1e46633e3d980d6d
IIIByoIII <robertkiss26@gmail.com>
2017-06-01 15:50:30 +0300

    First passed test for HungryGoats

--- HungryGoats/HungryGoats.cs
+++ HungryGoats/HungryGoats.cs
@@ -3,12 +3,23 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace HungryGoats
 {
+    //    În X zile Y capre mănâncă Z kg fân.
+    //Câte kg de fân mănâncă Q capre în W zile?
     [TestClass]
-    public class UnitTest1
+    public class HungryGoats
     {
         [TestMethod]
-        public void TestMethod1()
+        public void HayEatenByGoats()
         {
+            double hayEaten = CalculateEatenHay(2, 2, 4.0, 3, 3);
+            Assert.AreEqual(9.0, hayEaten);
+        }
+
+        double CalculateEatenHay(int xDays, int yGoats, double zHay, int wDays, int qGoats)
+        {
+            double hayEatenPerGoatPerDay = zHay / yGoats / xDays;
+            double hayForQGoatsInWDays = hayEatenPerGoatPerDay * qGoats * wDays;
+            return hayForQGoatsInWDays;
         }
     }
 }
