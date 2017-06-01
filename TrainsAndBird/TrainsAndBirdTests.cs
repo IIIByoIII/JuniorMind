17c0945662dfe17774ae3f11f0d4b90511ea7768
IIIByoIII <robertkiss26@gmail.com>
2017-06-01 13:44:29 +0300

    First fly distance of bird

--- TrainsAndBird/TrainsAndBirdTests.cs
+++ TrainsAndBird/TrainsAndBirdTests.cs
@@ -3,12 +3,24 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TrainsAndBird
 {
+    //    Două trenuri pornesc în același timp unul către celălalt cu o viteză constantă de X km/h.
+    //Când cele două trenuri au parcurs o pătrime din distanța inițială, o pasăre aflată pe primul tren pornește în zbor către al 2-lea tren cu o viteză de 2X.
+    //Imediat ce a ajuns la el se întoarce către primul și repetă asta până când cele două trenuri se întâlnesc.
+    //Care e distanța zburată de pasăre?
     [TestClass]
-    public class UnitTest1
+    public class TrainsAndBirdTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FirstFlyDistanceOfBird()
         {
+            decimal birdFlyDistance = CalculateBirdFlyDistance(60);
+            Assert.AreEqual(20, birdFlyDistance);
+        }
+
+        decimal CalculateBirdFlyDistance(decimal trainDistance)
+        {
+            // (trainDistance/2)*(2/3)
+            return trainDistance / 3;
         }
     }
 }
