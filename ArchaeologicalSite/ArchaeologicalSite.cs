66788f8b1251b899278db9aa87b18c66a35c9c5a
IIIByoIII <robertkiss26@gmail.com>
2017-06-05 11:53:33 +0300

    First test of ArchaeologicalSite

--- ArchaeologicalSite/ArchaeologicalSite.cs
+++ ArchaeologicalSite/ArchaeologicalSite.cs
@@ -4,11 +4,19 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ArchaeologicalSite
 {
     [TestClass]
-    public class UnitTest1
+    public class ArchaeologicalSite
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SurfaceOfBuilding()
         {
+            double minimumSurfaceOfBuilding = MinimumSurface (1, 1, 4, 1, 1, 4);
+            Assert.AreEqual(4.5, minimumSurfaceOfBuilding);
+        }
+
+        double MinimumSurface (double columnOneX, double columnOneY, double columnTwoX, double columnTwoY, double columnThreeX, double columnThreeY)
+        {
+            double areaOfBuilding = (((columnOneX * columnTwoY) - (columnTwoX * columnOneY)) + ((columnTwoX * columnThreeY) - (columnThreeX * columnTwoY)) + ((columnThreeX * columnOneY) - (columnOneX * columnThreeY))) / 2;
+            return areaOfBuilding;
         }
     }
 }
