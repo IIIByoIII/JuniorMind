32e8772ce5a7fa02eec10fad796188854eb4eaf7
IIIByoIII <robertkiss26@gmail.com>
2017-06-05 10:47:29 +0300

    First test of AthleteRepetitions

--- AthleteRepetitions/AthleteRepetitions.cs
+++ AthleteRepetitions/AthleteRepetitions.cs
@@ -7,8 +7,21 @@ namespace AthleteRepetitions
     public class AthleteRepetitions
     {
         [TestMethod]
-        public void TestMethod1()
+        public void RepetitionsDone()
         {
+            int repetitionsToDo = TotalRepetitions(3);
+            Assert.AreEqual(9, repetitionsToDo);
+        }
+
+        int TotalRepetitions (int numberOfRounds)
+        {
+            int currentNumberOfRepetitions = 0;
+            for (int i = 1; i < numberOfRounds; i++)
+            {
+                currentNumberOfRepetitions += 2 * i;
+            }
+            int totalNumberOfRepetitions = currentNumberOfRepetitions + numberOfRounds;
+            return totalNumberOfRepetitions;
         }
     }
 }
