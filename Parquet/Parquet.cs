5e3fc06f4c5cb588595271f3d15f6950796859f9
IIIByoIII <robertkiss26@gmail.com>
2017-06-05 11:21:21 +0300

    First test of Parquet

--- Parquet/Parquet.cs
+++ Parquet/Parquet.cs
@@ -4,11 +4,20 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Parquet
 {
     [TestClass]
-    public class UnitTest1
+    public class Parquet
     {
         [TestMethod]
-        public void TestMethod1()
+        public void NeededParquetSurface()
         {
+            double surfaceOfParquetNeeded = ParquetSurfaceNeeded(5, 6);
+            Assert.AreEqual(34.5, surfaceOfParquetNeeded);
+        }
+
+        double ParquetSurfaceNeeded (double roomLength, double roomWidth)
+        {
+            double roomSurface = roomLength * roomWidth;
+            double parquetSurface = roomSurface + (roomSurface * 15 / 100);
+            return parquetSurface;
         }
     }
 }
